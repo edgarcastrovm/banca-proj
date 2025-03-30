204441cc11542712e2993e39b44b46b0fcd23185
Edgar Castro <edgar.castro.vm@gmail.com>
2025-03-29 21:37:06 -0500

    Se implementa test Unitario

--- api-customer/src/test/java/com/banca/customer/CustomerApiApplicationTests.java
+++ api-customer/src/test/java/com/banca/customer/CustomerApiApplicationTests.java
@@ -1,13 +1,32 @@
 package com.banca.customer;
 
+import com.banca.customer.service.ClienteService;
+import com.banca.utils.db.entity.Cliente;
+import com.banca.utils.db.entity.Persona;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
+@EntityScan(basePackages = "com.banca.utils")
 class CustomerApiApplicationTests {
+	@Autowired
+	private ClienteService clienteService;
 
 	@Test
-	void contextLoads() {
-	}
+	public void testSaveClienteConPersonaInvalida() {
+		Cliente cliente = new Cliente();
+		cliente.setPersona(new Persona());
+		Assertions.assertThrows(DataIntegrityViolationException.class, () -> {
+			clienteService.save(cliente);
+		});
 
+		assertNull(cliente.getId());
+		assertNull(cliente.getPersona().getId());
+	}
 }
